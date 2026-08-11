f2d7ed8e9784fda7dc43f7ea37b85868b9425088
DexZed <144045080+DexZed@users.noreply.github.com>
2026-08-11 22:11:51 +0600

    login page

--- fixitnowfrontend/src/app/auth/login/page.tsx
+++ fixitnowfrontend/src/app/auth/login/page.tsx
@@ -1,11 +1,80 @@
+"use client"
+import { authClient } from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import z from "zod";
+
+const signInSchema = z.object({
+    email: z.string(),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+    role: z.enum(["admin", "customer", "technician"]).default("customer").optional(),
+})
+
+type SignInSchema = z.infer<typeof signInSchema>;
 
 
-function LoginPage() {
+function SignInPage() {
+    const { register, handleSubmit, formState: { errors } } = useForm<SignInSchema>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const router = useRouter();
+
+    const submitHandler = async (data: SignInSchema) => {
+        await authClient.signIn.email({
+            email: data.email,
+            password: data.password,
+        }, {
+            onSuccess: () => {
+                toast.success("User logged in successfully");
+                router.push("/");
+            },
+            onError: (error) => {
+                toast.error("Error logging in user");
+                console.error("Error in login ", error?.error?.message || "Something went wrong");
+            }
+        });
+    };
+
+
     return (
         <div>
-            <h1>Login</h1>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+
+                <div className="card-body">
+                    <h2 className="text-xl font-bold text-neutral-200 card-title">
+                        Sign In
+                    </h2>
+                    <p className="mt-2 max-w-sm text-sm text-neutral-300 ">
+                        Please fill out the form to sign in to your account
+                    </p>
+
+                    <form onSubmit={handleSubmit(submitHandler)}>
+
+                        <fieldset className="fieldset">
+                            <label className="label" htmlFor="email">Email</label>
+                            <input type="email" id="email" {...register("email")} className="input input-bordered input-info" placeholder="Email" />
+                            {errors.email && <span className="label text-red-500">{errors.email.message}</span>}
+                        </fieldset>
+
+                        <fieldset className="fieldset">
+                            <label className="label" htmlFor="password">Password</label>
+                            <input type="password" id="password" {...register("password")} className="input input-bordered input-info" placeholder="Password" />
+                            {errors.password && <span className="label text-red-500">{errors.password.message}</span>}
+                        </fieldset>
+
+                        <button type="submit" className="btn btn-neutral mt-4">Login</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
 
-export default LoginPage;
+export default SignInPage;
